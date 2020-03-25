f485980c9f77ee7088995f1bf93408607d480aa1
unknown <hendikedison09@gmail.com>
2020-03-25 09:42:47 +0700

    webdesign_chocoffee

---
+++ application/views/templates/header.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="<?= base_url('assets/') . "style.css" ?>">
+
+  <!-- FONT -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+
+  <!-- Fontawesome -->
+  <script src="https://kit.fontawesome.com/25106969d6.js"></script>
+
+	<title></title>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+		<div class="container">
+			<a class="navbar-brand" href="#">
+				<img src="assets/img/logo-putih.png" alt="">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+				aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav  ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#about">Tentang Kami <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#paket">Paket Pilihan</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#pelayanan">ALur Pelayanan</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#kontak">Kontak</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
---
+++ application/views/v_home.php
@@ -0,0 +1,282 @@
+<div class="halaman-utama">
+    <!-- JUMBOTRON -->
+    <div class="jumbotron">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="container">
+                    <div class="judul">
+                        <h1 class="judul-1">Halo, <b>Chocofiers!</b></h1>
+                        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+                        <p class="line float-left">
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-md-8 ml-2">
+                <button type="button" class="btn btn-outline-dark">Lebih Lengkap </button>
+                <button type="button" class="btn btn-outline-danger">Langsung Pesan</button>
+            </div>
+        </div>
+    </div>
+    <!-- JUMBOTRON END-->
+
+    <!-- ABOUT US -->
+    <div class="tentang-kami" id="about">
+        <div class="container">
+            <div class="content1">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h2><b>FRANCHISE <span class="label-colored">CHOCOFFEE</span></b></h2>
+                    </div>
+                    <div class="col-md-6 mt-5">
+                        <div class="bordered-right section-title">
+                            <small>Pertanyaanya</small>
+                            <h3><b>Kenapa <span class="label-colored"> Memilih Kami</span></b></h3>
+                            <p>Design Team Leader & Product Development PT. Eurogate Malaysia: Higs on International Group</p>
+                        </div>
+                        <div class="list">
+                            <ul>
+                                <li>
+                                    <i class="far fa-check-circle"></i>
+                                    Kemudahan dalam pemesanan
+                                </li>
+                                <li>
+                                    <i class="far fa-check-circle"></i>
+                                    Kehandalan pelayan dalam memberikan pelayanan
+                                </li>
+                                <li>
+                                    <i class="far fa-check-circle"></i>
+                                    Ramah terhadap pelanggan
+                                </li>
+                                <li>
+                                    <i class="far fa-check-circle"></i>
+                                    Mengutamakan profesionalitas
+                                </li>
+                                <li>
+                                    <i class="far fa-check-circle"></i>
+                                    Cepat, mudah, dan terpercaya
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ABOUT US END-->
+
+    <!-- PAKET PILIHAN -->
+    <div class="paket-pilihan" id="paket">
+        <div class="container">
+            <div class="">
+                <h2><b><span class="label-colored">PAKET</span> PILIHAN</b></h2>
+                <p class="bordered-bottom float-left"></p>
+                <br>
+                <p>Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting.</p>
+            </div>
+            <div class="content2">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="pricingTable11">
+                            <div class="pricingTable-header">
+                                <i class="fas fa-box-open"></i>
+                                <div class="price-value"> $10.00 <span class="month">per month</span> </div>
+                            </div>
+                                <h3 class="heading">Standard</h3>
+                                <div class="pricing-content">
+                                    <ul>
+                                        <li><b>50GB</b> Disk Space</li>
+                                        <li><b>50</b> Email Accounts</li>
+                                        <li><b>50GB</b> Monthly Bandwidth</li>
+                                        <li><b>10</b> subdomains</li>
+                                        <li><b>15</b> Domains</li>
+                                    </ul>
+                                </div>
+                                <div class="pricingTable-signup">
+                                    <a href="#">PESAN <i class="fas fa-shopping-cart"></i> </a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="pricingTable11">
+                            <div class="pricingTable-header">
+                                <i class="fas fa-box-open"></i>
+                                <div class="price-value"> $10.00 <span class="month">per month</span> </div>
+                            </div>
+                                <h3 class="heading">Standard</h3>
+                                <div class="pricing-content">
+                                    <ul>
+                                        <li><b>50GB</b> Disk Space</li>
+                                        <li><b>50</b> Email Accounts</li>
+                                        <li><b>50GB</b> Monthly Bandwidth</li>
+                                        <li><b>10</b> subdomains</li>
+                                        <li><b>15</b> Domains</li>
+                                    </ul>
+                                </div>
+                                <div class="pricingTable-signup">
+                                    <a href="#">pesan <i class="fas fa-shopping-cart"></i> </a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="pricingTable11">
+                            <div class="pricingTable-header">
+                                <i class="fas fa-box-open"></i>
+                                <div class="price-value"> $10.00 <span class="month">per month</span> </div>
+                            </div>
+                                <h3 class="heading">Standard</h3>
+                                <div class="pricing-content">
+                                    <ul>
+                                        <li><b>50GB</b> Disk Space</li>
+                                        <li><b>50</b> Email Accounts</li>
+                                        <li><b>50GB</b> Monthly Bandwidth</li>
+                                        <li><b>10</b> subdomains</li>
+                                        <li><b>15</b> Domains</li>
+                                    </ul>
+                                </div>
+                                <div class="pricingTable-signup">
+                                    <a href="#">pesan <i class="fas fa-shopping-cart"></i> </a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="pricingTable11">
+                            <div class="pricingTable-header">
+                                <i class="fas fa-box-open"></i>
+                                <div class="price-value"> $10.00 <span class="month">per month</span> </div>
+                            </div>
+                                <h3 class="heading">Standard</h3>
+                                <div class="pricing-content">
+                                    <ul>
+                                        <li><b>50GB</b> Disk Space</li>
+                                        <li><b>50</b> Email Accounts</li>
+                                        <li><b>50GB</b> Monthly Bandwidth</li>
+                                        <li><b>10</b> subdomains</li>
+                                        <li><b>15</b> Domains</li>
+                                    </ul>
+                                </div>
+                                <div class="pricingTable-signup">
+                                    <a href="#">pesan <i class="fas fa-shopping-cart"></i> </a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- PAKET PILIHAN END-->
+
+    <!-- TIMELINE PEMESANAN -->
+    <div class="timeline" id="pelayanan">
+        <div class="container">
+            <div class="">
+                <h2><b><span class="label-colored">ALUR</span> PELAYANAN</b></h2>
+                <p class="bordered-bottom float-left"></p>
+                <br>
+            </div>
+            <div class="content3">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="main-timeline">
+                            <a href="#" class="timeline">
+                                <div class="timeline-icon"><i class="fa fa-shopping-cart"></i></div>
+                                <div class="timeline-content">
+                                    <h3 class="title">Pemesanan</h3>
+                                    <p class="description">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus officiis quod! Aperiam eveniet nam nostrum odit quasi ullam voluptatum.
+                                    </p>
+                                </div>
+                            </a>
+                            <a href="#" class="timeline">
+                                <div class="timeline-icon"><i class="fa fa-rocket"></i></div>
+                                <div class="timeline-content">
+                                    <h3 class="title">Web Developer</h3>
+                                    <p class="description">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus officiis quod! Aperiam eveniet nam nostrum odit quasi ullam voluptatum.
+                                    </p>
+                                </div>
+                            </a>
+                            <a href="#" class="timeline">
+                                <div class="timeline-icon"><i class="fa fa-briefcase"></i></div>
+                                <div class="timeline-content">
+                                    <h3 class="title">Web Designer</h3>
+                                    <p class="description">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus officiis quod! Aperiam eveniet nam nostrum odit quasi ullam voluptatum.
+                                    </p>
+                                </div>
+                            </a>
+                            <a href="#" class="timeline">
+                                <div class="timeline-icon"><i class="fa fa-mobile"></i></div>
+                                <div class="timeline-content">
+                                    <h3 class="title">Web Developer</h3>
+                                    <p class="description">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus officiis quod! Aperiam eveniet nam nostrum odit quasi ullam voluptatum.
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- TIMELINE PEMESANAN -->
+
+    <!-- CONTACT -->
+    <div class="contact" id="kontak">
+        <div class="container">
+            <div class="">
+                <h2><b><span class="label-colored">KONTAK</span> KAMI</b></h2>
+                <p class="bordered-bottom float-left"></p>
+                <br>
+            </div>
+            <div class="content4">
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <div class="card border-0">
+                            <div class="card-body text-center">
+                                <i class="fa fa-phone fa-4x mb-3" aria-hidden="true"></i>
+                                <br>
+                                <br>
+                                <br>
+                                <p>+8801683615582</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card border-0">
+                            <div class="card-body text-center">
+                                <i class="fas fa-map-marked-alt fa-4x mb-3" aria-hidden="true"></i>
+                                <br>
+                                <br>
+                                <br>
+                                <p>Kedungsigit, Trenggalek</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card border-0">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope-open fa-4x mb-3" aria-hidden="true"></i>
+                                <br>
+                                <br>
+                                <br>
+                                <p>chocoffeekedungsigit@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- CONTACT END -->
+
+</div>
